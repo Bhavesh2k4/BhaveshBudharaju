dd46bceb5a0815c90b941ea1875d1058b36a71d5
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2024-04-17 07:46:02 +0530

    update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const dbConfig=require("./config/dbConfig");
 const portfolioRoute=require("./routes/portfolioRoute");
 const cors = require('cors');
 app.use(express.json());
-app.use("https://bhavesh-portfolio-backend.vercel.app/api/portfolio",portfolioRoute);
+app.use("/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5050;
 const allowedOrigins = ['https://bhavesh-budharaju-portfolio.vercel.app'];
 app.use(
@@ -35,5 +35,9 @@ if (process.env.NODE_ENV === 'production'){
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
+});
+
+app.use("/",(req,res)=>{
+    res.status(404).send("server is running");
 });
 
