6c82007ab555c732a669cd56b5a28e03f474195e
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2024-04-17 07:25:50 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -3,9 +3,26 @@ const app=express();
 require("dotenv").config();
 const dbConfig=require("./config/dbConfig");
 const portfolioRoute=require("./routes/portfolioRoute");
+const cors = require('cors');
 app.use(express.json());
 app.use("https://bhavesh-portfolio-backend.vercel.app/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5050;
+const allowedOrigins = ['https://bhavesh-budharaju-portfolio.vercel.app'];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      // Allow requests from the specified origins
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 //for hosting
 /*const path=require("path");
