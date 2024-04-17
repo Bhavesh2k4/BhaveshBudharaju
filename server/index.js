f494aa4377ddc5695415c7a94c5d300ccf45af03
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2024-04-17 15:19:02 +0530

    minor errors

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 require("./config/dbConfig");
 const portfolioRoute=require("./routes/portfolioRoute");
 const cors = require('cors');
-app.use(express.json());
-app.use("/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5050;
 
 app.use(
@@ -15,10 +13,11 @@ app.use(
       credentials: true,
 }));
 
+app.use("/api/portfolio",portfolioRoute);
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
 });
-
+app.use(express.json());
 app.use("/",(req,res)=>{
     res.status(404).send("server is running");
 });
