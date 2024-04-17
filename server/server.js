5248f0a54bbd34961c7694e313f397dcf93f31a8
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2024-04-17 07:51:48 +0530

    minor errors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use("/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5050;
-const allowedOrigins = ['https://bhavesh-budharaju-portfolio.vercel.app'];
+const allowedOrigins = [''];
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -23,15 +23,6 @@ app.use(
     },
   })
 );
-
-//for hosting
-/*const path=require("path");
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"client/build")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"client/build/index.html"));
-    });
-}*/
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
